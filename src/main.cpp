5034b43284e7ed9f1205a1b2452f6d994dad6d1c
Diego <diegortizte@gmail.com>
2024-01-27 19:48:06 -0300

    feat: create assets class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "EntityManager.h"
+#include "Assets.h"
 
 int main() {
   EntityManager manager; 
@@ -19,5 +20,42 @@ int main() {
   if(e->hasComponent<CTransform>()) std::cout << e->hasComponent<CTransform>() << "\n";
   //if(e->hasComponent<CBoundingBox>()) std::cout << e->hasComponent<CBoundingBox>() << "\n";
 
+  sf::RenderWindow window(sf::VideoMode(1920, 1080), "mega mario", sf::Style::Fullscreen);
+
+  Assets assets;
+  assets.addTexture("ground", "../bin/assets/mario/ground.png");
+  assets.addTexture("brick", "../bin/assets/mario/brick.png");
+  assets.addFont("mario", "../bin/fonts/super-mario-bros-nes.ttf");
+  assets.addTexture("pipe", "../bin/assets/mario/tall pipe.png");
+
+  while(window.isOpen()) {
+    sf::Event event;
+    while(window.pollEvent(event)) {
+      if(event.type == sf::Event::Closed) window.close();
+    }
+    window.clear(sf::Color(0, 87, 217));
+    sf::Texture tex = assets.getTexture("ground");
+    sf::Sprite sprite(tex);
+    sprite.setScale(64.0 / tex.getSize().x, 64.0 / tex.getSize().y); 
+
+    sf::Texture tex2 = assets.getTexture("brick");
+    sf::Sprite sprite2(tex2);
+    sprite2.setScale(64.0 / tex2.getSize().x, 64.0 / tex2.getSize().y); 
+    sprite2.setPosition(300, 300);
+
+    sf::Text text("poseemos assets", assets.getFont("mario"), 34);
+    text.setPosition(500, 500);
+
+    sf::Texture tex3 = assets.getTexture("pipe");
+    sf::Sprite sprite3(tex3);
+    sprite3.setScale(128.0 / tex3.getSize().x, 128.0 / tex3.getSize().x); 
+    sprite3.setPosition(800, 800);
+
+    window.draw(sprite);
+    window.draw(sprite2);
+    window.draw(sprite3);
+    window.draw(text);
+    window.display();
+  }
   return 0;
 }
