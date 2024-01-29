9d500177d6b999f276ea6c92ea470e3ab9464400
Diego <diegortizte@gmail.com>
2024-01-29 00:34:59 -0300

    feat: create scene class & scene_play class

---
+++ src/Scene_Play.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Scene.h"
+
+class Scene_Play : public Scene {
+    struct PlayerConfig {
+        float X, Y, CX, CY, SPEED, MAXSPEED, JUMP, GRAVITY;
+        std::string WEAPON;
+    };
+
+   protected:
+    std::shared_ptr<Entity> m_player;
+    std::string m_levelPath;
+    PlayerConfig m_playerConfing;
+    bool m_drawTextures = true;
+    bool m_drawCollision = false;
+    bool m_drawGrid = false;
+    const Vec2 m_gridSize = {64, 64};
+    void init(std::string levelPath);
+    void onEnd();
+
+   public:
+    Scene_Play(GameEngine* gameEngine = nullptr, std::string levelPath = "");
+    void update();
+    void sDoAction(const Action& action);
+    void sRender();
+};
